6ace36cba70fdbe75dfe1eeccc270b4152bb93d6
Davi Gabriel <47298896+daviziih@users.noreply.github.com>
2023-09-02 12:51:54 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,38 @@
+F = 0;
+Celsius = 20;
+C = 18;
+
+milhas = 0;
+quilometros = 20;
+q = 0;
+
+metros = 0;
+pes = 20;
+pe = 0;
+
+F = (9 * Celsius + 160) / 5
+C = (F - 32) * (5 / 9)
+
+milhas = quilometros * 0.621371
+q = milhas * 1.60934
+
+metros = pes * 0,3048
+pe = metros * 3,28084
+
+print("Fahrenheit: ", F)
+print("Celsius: ", C)
+print("Milhas: ", milhas)
+print("Quilometros: ", q)
+print("Metros: ", metros)
+print("Pes: ", pe)
+
+
+x = [1, 2, 3]
+print(x)
+
+x = 5
+y = "10"
+
+result = x + int(y)
+
+print(result)
